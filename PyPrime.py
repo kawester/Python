a08052de1bbe9af39cf57181bcf6f853937513b7
Kurt Westerman <kawester@uark.edu>
2020-01-27 21:05:09 -0600

    Update PyPrime.py
    cleaning up for readability

--- PyPrime.py
+++ PyPrime.py
@@ -1,20 +1,24 @@
 def isPrime(x):
-	if(x<2):return False
-	for i in range(2,x):
-		if(x%i==0):
-			return False
-	return True
+    if x < 2:
+        return False
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
 
-while(True):
-	try:
-		num=int(input("Give me an Integer and I will check if any number between 1 and it inclusive is prime."))
-	except:
-		print("Please use a valid Integer.")
-		continue
-	for i in range(num):
-		if(isPrime(i)):
-			print(str(i)+" is prime.")
-	if(not isPrime(num)):
-			print(str(num)+" is not prime.")
-	flag=input("continue? (y/n)")
-	if(flag.upper()=="N" or flag.upper()=="NO"): break
+
+while True:
+    try:
+        num = int(input("Give me an Integer and I will check if any number between 1 and it inclusive is prime."))
+    except ValueError:
+        print("Please use a valid integer.")
+        continue
+    for i in range(num):
+        if isPrime(i):
+            print(str(i) + " is prime.")
+    if not isPrime(num):
+        print(str(num) + " is not prime.")
+    flag = input("continue? (y/n)")
+    if flag.upper() == "N" or flag.upper() == "NO":
+        break
+	
